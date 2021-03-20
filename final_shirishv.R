e1e5b817cbc3ab4730095ac619f4646bb66f23c4
shirishverma <70457478+shirishverma@users.noreply.github.com>
2021-03-19 20:53:35 -0400

    Second commit
    Changed formatting of large numbers i.e. now numbers have commas; added a new tab called MapView that has a leaflet plot. Things to work on: larger color palette, one more layer.

--- final_shirishv.R
+++ final_shirishv.R
@@ -9,6 +9,7 @@ library(plotly)
 library(shinythemes)
 library(tools)
 library(stringr)
+library(randomcoloR)
 
 # setting working directory
 # setwd("C:/Users/shiri/Dropbox/CMU - 4th sem/R Shiny/final_shirishv")
@@ -66,14 +67,43 @@ ui <- navbarPage("Allegheny County 911 EMS Dashboard",
                               br(),
                               tabsetPanel(tabPanel("Plot 1: Count of calls by priority",
                                                    tabsetPanel(tabPanel("Plot", plotlyOutput("barplot")),
-                                                               tabPanel("Data table", DT::dataTableOutput("agg.table")))),
+                                                               tabPanel("Data table", DT::dataTableOutput("agg.table"),
+                                                                        downloadButton("download_dt_1", "Download data")))),
                                           tabPanel("Plot 2: Overtime trend in the selected city",
                                                    tabsetPanel(tabPanel("Plot", plotlyOutput("lineplot")),
-                                                               tabPanel("Data table", DT::dataTableOutput("city.table"))))
+                                                               tabPanel("Data table", DT::dataTableOutput("city.table"),
+                                                                        downloadButton("download_dt_2", "Download data"))))
                                           )
                             )
                           )
                  ),
+                 # Map Panel
+                 tabPanel("Map View",
+                          sidebarLayout(
+                            sidebarPanel(width = 3,
+                               h4(strong("User Inputs")),
+                               hr(style = "border-top: 1px solid #000000;"),
+                               
+                               # Select year
+                               selectInput("year_multi",
+                                           "Year(s):",
+                                           choices = unique(sort(ems$call_year)),
+                                           selected = c("2020"),
+                                           selectize = T,
+                                           multiple = T),
+                               
+                               # Select the Priority type to highlight in the map
+                               checkboxGroupInput(inputId = "priorities",
+                                                  label = "Select priority(ies):",
+                                                  choices = unique(sort(ems$priority))),
+                               actionLink("selectall", "Select all priorities")
+                                         ),
+                            mainPanel(
+                              # Map Page
+                              leafletOutput("leaflet", height = "80vh")
+                            )
+                            )
+                          ),
                  # Data Table Pannel
                  tabPanel("Raw Data",
                           fluidPage(
@@ -83,18 +113,20 @@ ui <- navbarPage("Allegheny County 911 EMS Dashboard",
 )
 
 # Defining server function required to create plots and value boxes -----
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   # Making three valueboxes
   output$total_calls <- renderValueBox({
     num <- nrow(ems)
+    num <- format(num, big.mark=",",small.mark=".", small.interval=3)
     
-    valueBox(subtitle = "Total Calls", value = num, icon = icon("phone-square"), color = "blue")
+    valueBox(subtitle = "Total Calls in 6 years", value = num, icon = icon("phone-square"), color = "blue")
   })
   
   output$high_priority_calls <- renderValueBox({
     new_table <- table(ems$priority)
     num <- new_table[['E0']]
+    num <- format(num, big.mark=",",small.mark=".", small.interval=3)
     
     valueBox(subtitle = "Calls requiring E0 priority", value = num, icon = icon("ambulance"), color = "blue")
   })
@@ -121,6 +153,14 @@ server <- function(input, output) {
   # Aggregated data table
   output$agg.table <- DT::renderDataTable({emsagg()})
   
+  # Download the data in data table
+  output$download_dt_1 <- downloadHandler(
+    filename = function(){"count_of_calls_by_prority_per_quarter.csv"},
+    content = function(fname){
+      write.csv(emsagg(), fname)
+    }
+  )
+  
   # Making a bar plot using the above aggregated data
   output$barplot <- renderPlotly({
     ggplot(emsagg(), aes(x = call_year, y = values, fill = call_quarter)) +
@@ -128,6 +168,7 @@ server <- function(input, output) {
       theme_bw() +
       scale_x_continuous(breaks = seq(input$slider[1],input$slider[2],1),
                           limits = c(input$slider[1],input$slider[2])) +
+      scale_y_continuous(labels = scales::comma) +
       scale_fill_manual("Quarters", values = c("#fdbf60", "#f07d60", "#d2449a", "#78347c")) +
       labs(x = "Years", y = "Number of calls") +
       theme(axis.title.x = element_text(face = "bold"), 
@@ -152,6 +193,14 @@ server <- function(input, output) {
   # City data table
   output$city.table <- DT::renderDataTable({ems_city()})
   
+  # Download the data in data table
+  output$download_dt_2 <- downloadHandler(
+    filename = function(){"count_of_calls_by_city_per_year.csv"},
+    content = function(fname){
+      write.csv(emsagg(), fname)
+    }
+  )
+  
   # Making a line plot using the above data
   output$lineplot <- renderPlotly({
     ggplot(ems_city(), aes(x = call_year, y = values, color = priority)) +
@@ -159,6 +208,7 @@ server <- function(input, output) {
       theme_bw() +
       scale_x_continuous(breaks = seq(input$slider[1],input$slider[2],1), 
                          limits = c(input$slider[1],input$slider[2])) +
+      scale_y_continuous(labels = scales::comma) +
       labs(x = "Years", y = "Number of calls", color = "Priorities") +
       theme(axis.title.x = element_text(face = "bold"), 
             axis.title.y = element_text(face = "bold"),
@@ -166,8 +216,71 @@ server <- function(input, output) {
             legend.background = element_rect(fill = "lightgray"))
   })
   
+  # if-else function to check all priority codes
+  observe({
+    if(input$selectall == 0) return(NULL) 
+    else if (input$selectall%%2 == 0)
+    {
+      updateCheckboxGroupInput(session,
+                               "priorities",
+                               "Select prioritiy(ies):",
+                               choices = unique(sort(ems$priority)))
+    }
+    else
+    {
+      updateCheckboxGroupInput(session,"priorities",
+                               "Select prioritiy(ies):",
+                               choices = unique(sort(ems$priority)),
+                               selected = unique(sort(ems$priority)))
+    }
+  })
+  
+  # creating subset for the map
+  mapinput <- reactive({
+    # Selected years
+    if (length(input$year_multi) > 0) {
+    map_filter <- subset(ems, call_year %in% input$year_multi)
+    }
+    # Priority types
+    if (length(input$priorities) > 0) {
+      map_filter <- subset(map_filter, priority %in% input$priorities)
+    }
+    # randomly sampling 500 points
+    if(length(map_filter) > 1000) {
+      map_filter <- sample_n(map_filter,1000) 
+    }
+    return(map_filter)
+  })
+  
+  
+  # Map
+  output$leaflet <- renderLeaflet({
+    leaflet() %>%
+      addTiles(urlTemplate = "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", 
+               attribution = "Google", group = "Google") %>%
+      addProviderTiles("Stamen.Toner", group = "Toner") %>%
+      setView(-79.9800, 40.4700, 9) %>%
+      addLayersControl(baseGroups = c("Google", "Toner"))
+  })
+  
+  pal <- colorFactor(
+    palette = distinctColorPalette(20),
+    domain = ems$priority)
+  
+  # Replace layer with filtered data
+  observe({
+    map_data <- mapinput()
+    leafletProxy("leaflet", data = map_data) %>%
+      clearMarkers() %>%
+      clearGroup(group = "map_data") %>%
+      addCircleMarkers(lng = ~census_block_group_center__x,
+                       lat = ~census_block_group_center__y, radius = 1.5,
+                       color = ~pal(priority), group = "map_data") %>%
+      addLegend(position = "bottomright" , pal = pal, values = ems$priority, title = "Priority type")
+  })
+  
   # Raw Data table ----------------------------------------------
-  output$raw.table <- DT::renderDataTable({ems[2:13]})
+  output$raw.table <- DT::renderDataTable({ems[2:13]}, options = list(scrollX = TRUE))
   
 }
 
